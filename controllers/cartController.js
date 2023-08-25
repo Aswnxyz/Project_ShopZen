37db348d4dc573058c12a48cb507e50e2d63d571
Aswin <aswinanith007@gmail.com>
2023-08-25 11:39:49 +0530

    Version 10

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -249,7 +249,7 @@ module.exports = {
       console.log(cartItems);
 
       let cartTotal = cartSubTotal;
-      if (afterDiscount>=0) {
+      if (afterDiscount || afterDiscount === 0) {
         cartTotal = afterDiscount;
       }
 
